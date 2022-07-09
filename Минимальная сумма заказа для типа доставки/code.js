23b8cad73a56429335b2c6814c378d69ce8129f9
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2022-07-09 11:50:12 +0600

    .

--- Минимальная сумма заказа для типа доставки/code.js
+++ Минимальная сумма заказа для типа доставки/code.js
@@ -1,20 +1,23 @@
-//Название типа доставки
-let myDeliveryName = 'Самовывоз'
-//Минимальная сумма заказа для нашего типа доставки
-let myMinOrder = 0
+window.onload = ()=>{
 
-let minorder = tcart_minorder
-let delivery = document.querySelector('.t-radio__wrapper-delivery')
-let minlabel = document.querySelector('.t706__minimal')
+    //Название типа доставки
+    let myDeliveryName = 'Самовывоз'
+    //Минимальная сумма заказа для нашего типа доставки
+    let myMinOrder = 0
 
-delivery.addEventListener('change', function(){
-    let delivery_val = delivery.querySelector('input:checked').value
+    let minorder = tcart_minorder
+    let delivery = document.querySelector('.t-radio__wrapper-delivery')
+    let minlabel = document.querySelector('.t706__minimal')
 
-    if (delivery_val == myDeliveryName){
-        tcart_minorder = myMinOrder
-        minlabel.style.display = 'none'
-    }else{
-        tcart_minorder = minorder
-        minlabel.style.display = 'block'
-    }
-})
+    delivery.addEventListener('change', ()=>{
+        let delivery_val = delivery.querySelector('input:checked').value
+
+        if (delivery_val == myDeliveryName){
+            tcart_minorder = myMinOrder
+            minlabel.style.display = 'none'
+        }else{
+            tcart_minorder = minorder
+            minlabel.style.display = 'block'
+        }
+    })
+}
