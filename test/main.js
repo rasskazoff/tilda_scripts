3c28cbce5abcf39d7f4ab468d23f75d62b990e38
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2022-12-16 23:53:02 +0600

    .

--- test/main.js
+++ test/main.js
@@ -1,1 +1,14 @@
-alert('Скрипт подключен')
+console.log('Скрипт подключен')
+
+function t_ready(e){"loading"!=document.readyState?e():document.addEventListener("DOMContentLoaded",e)}
+
+t_ready(()=>{
+    let shapeRotate = document.querySelector('.rotate .tn-atom')
+    shapeRotate.animate([
+        { transform: 'rotate(0)' },
+        { transform: 'rotate(360deg)' }
+    ], {
+        duration: 5000,
+        iterations: Infinity
+    })
+})
