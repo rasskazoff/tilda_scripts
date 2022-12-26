da6c9f7284ca301adffd3db6a4223155d74f8869
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2022-12-27 02:07:38 +0600

    .

--- perimetr-remonta/quiz-landing/scripts.js
+++ perimetr-remonta/quiz-landing/scripts.js
@@ -1,10 +1,36 @@
 function t_ready(e){"loading"!=document.readyState?e():document.addEventListener("DOMContentLoaded",e)}
 
-//текст вокруг кнопки 
+//квиз
 try {
     t_ready(()=>{
-        const quiz = document.querySelector('.uc-quiz')
-        const quizNew = document.querySelector('#quiz')
-        quizNew.append(quiz)
+        if (innerWidth > 959){
+            const quiz = document.querySelector('.uc-quiz')
+            const quizNew = document.querySelector('#quiz')
+            quizNew.append(quiz)
+        }
+    })
+}catch(err){console.log(err)}
+
+//шапка
+try {
+    t_ready(()=>{
+        const menu = document.querySelector('.uc-fixMenu')
+        menu.classList.add('fixed')
+        menu.classList.add('pinned')
+
+        const showMenu = ()=>{
+            if(window.pageYOffset > 333){
+                setTimeout(()=>{
+                    menu.classList.remove('fixed')
+                },100)
+            }else{
+                menu.classList.add('fixed')
+            }
+        }
+        showMenu()
+        document.addEventListener('scroll',()=>{
+            showMenu()
+        })
+
     })
 }catch(err){console.log(err)}
