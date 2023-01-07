a0e6cafaa2bccf90506e6beec8b36ce2c45faf11
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2023-01-08 02:53:54 +0600

    .

--- test/test.js
+++
@@ -1,6 +0,0 @@
-$.get('http://localhost:4000/api/waRender?id=89994704050', function(res) {
-    const elem = document.createElement('div')
-    elem.innerHTML = res.data
-    document.body.appendChild(elem)
-})
-    
